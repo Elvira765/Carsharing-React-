b16053dae11e9a95c8f76f0c1d171cff7961c867
Elvira Sadrieva <sadrievae18@gmail.com>
2020-04-22 13:36:49 +0400

    Updates

---
+++ precache-manifest.bbe4f735adfabc3357f7958025b5fd2c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65e43db43f6b8cdf44137f880c91456",
+    "url": "/Carsharing/index.html"
+  },
+  {
+    "revision": "44c78265f667b066e835",
+    "url": "/Carsharing/static/css/main.b1ddaa2b.chunk.css"
+  },
+  {
+    "revision": "072415ae29d0d7d66492",
+    "url": "/Carsharing/static/js/2.c6e1195d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Carsharing/static/js/2.c6e1195d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c78265f667b066e835",
+    "url": "/Carsharing/static/js/main.63c76ee4.chunk.js"
+  },
+  {
+    "revision": "3d9ddfaeb4fcb067e850",
+    "url": "/Carsharing/static/js/runtime-main.ba008bf8.js"
+  },
+  {
+    "revision": "9f836a1d4eaf65e22c94ccc14a147fd6",
+    "url": "/Carsharing/static/media/Roboto-Bold.9f836a1d.woff"
+  },
+  {
+    "revision": "bd474c1a659d6896646aae18fb603cce",
+    "url": "/Carsharing/static/media/Roboto-Bold.bd474c1a.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/Carsharing/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "7350337b673e86f2d62ee0eb5761e1e2",
+    "url": "/Carsharing/static/media/Roboto-Medium.7350337b.woff"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Carsharing/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "1cfd1aa3b4fe4bba10c132acea9985af",
+    "url": "/Carsharing/static/media/Roboto-Regular.1cfd1aa3.woff"
+  }
+]);
